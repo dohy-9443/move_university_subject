bad1ba114af6f6e46d888f979fff2ab2a0a972f6
dohy-9443 <dong_hyun95@hanmail.net>
2024-12-09 20:54:15 +0900

    feat : use case 작성

---
+++ lib/domain/use_case/get_user.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:move_university_subject/core/use_case/use_cases.dart';
+import 'package:move_university_subject/domain/entity/entity.dart';
+import 'package:move_university_subject/domain/repository/repository.dart';
+
+///
+/// @Project name    : move_university_subject
+/// @Class           : get_user.
+/// @Created by      : baekdonghyun.
+/// Created On       : 2024. 12. 9..
+/// Description      : 
+///
+
+class GetUserUseCase implements IUseCase<List<UserEntity>, GetUsersParams> {
+  final IUserRepository _repository;
+
+  GetUserUseCase(this._repository);
+
+  @override
+  Future<List<UserEntity>> call(GetUsersParams params) async {
+    final result = await _repository.fetch(startAfter: params.startAfter);
+
+    if (result.isFailure) throw Exception(result.error?.message ?? 'USE CASE ERROR');
+
+    return result.data!;
+  }
+}
+
+class GetUsersParams {
+  final DocumentSnapshot? startAfter;
+
+  GetUsersParams(this.startAfter);
+}
