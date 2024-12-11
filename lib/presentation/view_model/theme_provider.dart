60e6ed6de0917d3257a54ccd156783546d078d91
dohy-9443 <dong_hyun95@hanmail.net>
2024-12-12 02:23:52 +0900

    refactor : code 정리

--- lib/presentation/view_model/theme_provider.dart
+++ lib/presentation/view_model/theme_provider.dart
@@ -17,20 +17,14 @@ class ThemeNotifier extends StateNotifier<ThemeMode> {
   ThemeNotifier() : super(ThemeMode.system);
 
   void toggleTheme(BuildContext context) {
-    print('현재 상태: $state');
-    // 현재 디바이스 테마 확인
     final platformBrightness = MediaQuery.of(context).platformBrightness;
 
-    // 시스템 테마인 경우 디바이스 상태에 따라 테마 변경
     if (state == ThemeMode.system) {
-      state = platformBrightness == Brightness.light
-          ? ThemeMode.dark
-          : ThemeMode.light;
+      state = platformBrightness == Brightness.light ? ThemeMode.dark : ThemeMode.light;
     } else {
-      // 현재 상태가 light/dark이면 반대로 변경
       state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
     }
-    print('변경된 상태: $state');
+
   }
 
   bool get isDarkMode => state == ThemeMode.dark;
